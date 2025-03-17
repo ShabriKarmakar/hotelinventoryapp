1e2308396d20f5e1ff88cdb20e69c3d4c4eb0888
ShabriKarmakar <sabari.karmakar@gmail.com>
2025-03-18 02:31:14 +0530

    pipes, Component communication, change Detection , Lifecycle hooks: NgOnInit, NgOnChnage, NgDocheck

---
+++ projects/hotelinventory/src/app/rooms-list/rooms-list.component.ts
@@ -0,0 +1,51 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnInit,
+  Output,
+  SimpleChanges
+} from '@angular/core';
+import {RoomList} from "../rooms/rooms";
+
+@Component({
+  selector: 'hinv-rooms-list',
+  templateUrl: './rooms-list.component.html',
+  styleUrls: ['./rooms-list.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+  //ChangeDetection Would only be triggered when something is chnages in parent or child component other than that its DEFAULT (Any chnage would trigger)
+  //Componet should not change the data internally
+  //The property should be immutatable (this.roomloist.push() won't work since it MODIFIED the roomList property)
+})
+export class RoomsListComponent implements OnInit, OnChanges {
+
+  //Dumb Component or Child Component (Cant take action on its own UNLESS it asks Parent Component using OUTPUT)
+  //Where you're just giving what to render (providing no data)
+
+  @Input() rooms: RoomList[] = [];    // Make this property as a valid html property on 'hinv-rooms-list'
+  //you will be using this in anywhere across components   <hinv-rooms-list [rooms]="roomList"></hinv-rooms-list> like such
+
+  @Input() title: string = '';
+
+  @Output() selectedRoom = new EventEmitter<RoomList>();  //Sending selected room
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  selectRoom(room: RoomList){
+    this.selectedRoom.emit(room);   //Emits selectd room which button is clicked
+
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
+    if(changes['title']){  //IF title property is changed
+      this.title = changes['title'].currentValue.toUpperCase();
+    }
+  }
+
+}
--- projects/hotelinventory/src/app/rooms/rooms.component.ts
+++ projects/hotelinventory/src/app/rooms/rooms.component.ts
@@ -13,47 +13,76 @@ export class RoomsComponent implements OnInit {
 
   hideRooms: boolean = false;
 
+  selectedRoom!: RoomList;
+
   rooms: Room = {
     totalRooms: 20,
     availableRooms: 10,
     bookedRooms: 5,
   }
+title = "Room List Start"
 
-  roomList: RoomList[] = [{
-    roomNumber:1,
-    roomType: 'Deluxe Room',
-    amenities: 'AC',
-    price: 500,
-    photos: "",
-    checkInTime: new Date('11-Nov-2021'),
-    checkOutTime: new Date('11-Nov-2021'),
-  },
-    {
-      roomNumber:2,
-      roomType: 'Deluxe Room',
-      amenities: 'AC',
-      price: 1000,
-      photos: "",
-      checkInTime: new Date('11-Nov-2021'),
-      checkOutTime: new Date('11-Nov-2021'),
-    },
-    {
-      roomNumber:3,
-      roomType: 'Private Suite',
-      amenities: 'AC',
-      price: 15000,
-      photos: "",
-      checkInTime: new Date('11-Nov-2021'),
-      checkOutTime: new Date('11-Nov-2021'),
-    }]
+  roomList: RoomList[] = []
 
   constructor() {
   }
 
   ngOnInit(): void {
+
+    this.roomList = [{
+      roomNumber: 1,
+      roomType: 'Deluxe Room',
+      amenities: 'AC',
+      price: 500,
+      photos: "",
+      checkInTime: new Date('11-Nov-2021'),
+      checkOutTime: new Date('11-Nov-2021'),
+      rating: 4.5
+    },
+      {
+        roomNumber: 2,
+        roomType: 'Deluxe Room',
+        amenities: 'AC',
+        price: 1000,
+        photos: "",
+        checkInTime: new Date('11-Nov-2021'),
+        checkOutTime: new Date('11-Nov-2021'),
+        rating: 3.2
+      },
+      {
+        roomNumber: 3,
+        roomType: 'Private Suite',
+        amenities: 'AC',
+        price: 15000,
+        photos: "",
+        checkInTime: new Date('11-Nov-2021'),
+        checkOutTime: new Date('11-Nov-2021'),
+        rating: 3.4
+
+      }]
   }
 
   toggle() {
     this.hideRooms = !this.hideRooms;
+    this.title = "Room List Toggled";
+  }
+
+  selectRoom(room: RoomList) {
+    this.selectedRoom = room;
+  }
+
+  addRoom() {
+    const room: RoomList = {
+      roomNumber: 4,
+      roomType: 'Deluxe Room',
+      amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
+      price: 500,
+      photos:"",
+      checkInTime: new Date('11-Nov-2021'),
+      checkOutTime: new Date('12-Nov-2021'),
+      rating: 4.5,
+    }
+    // this.roomList.push(room); //The property should be immutatable (this.roomloist.push() won't work since it MODIFIED the roomList property)
+    this.roomList = [...this.roomList, room]; //this creates a new instance (spread operaor to take the previous data and "room" is added in the array)
   }
 }
